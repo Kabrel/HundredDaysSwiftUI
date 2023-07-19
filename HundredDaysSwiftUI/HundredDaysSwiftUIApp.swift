a673697f42b6f76ccf88a86164067af572f1a648
Kabrel <kshutov666@gmail.com>
2023-07-19 21:33:43 +0300

    Initial Commit

---
+++ HundredDaysSwiftUI/HundredDaysSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredDaysSwiftUIApp.swift
+//  HundredDaysSwiftUI
+//
+//  Created by Константин Шутов on 19.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HundredDaysSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
